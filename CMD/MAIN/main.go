341a64f9ec9d9c1303db7243abc86edd9353807a
unknown <ganorkarmohit551@gmail.com>
2025-04-02 01:01:32 +0530

    Main File done

--- CMD/MAIN/main.go
+++ CMD/MAIN/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main (){}
+import (
+	"DistributedFileDBMaster/Package/Route"
+	"DistributedFileDBMaster/Package/Utility"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	Utility.InitializeMetaData()
+
+	Utility.InitiateSocketConnection()
+
+	Utility.InitiateBroadCast()
+
+	MuxRouter := mux.NewRouter()
+
+	Route.CustomRouter(MuxRouter)
+
+	http.Handle("/", MuxRouter)
+
+	http.ListenAndServe("localhost:9040", MuxRouter)
+}
---
+++ Package/Utility/InitialiseMetaData.go
@@ -0,0 +1,5 @@
+package Utility
+
+func InitializeMetaData() {
+
+}
